8ef89f886afab0c3f70ff48f2873a1d9d3835635
Iona M <51836963+Metodist@users.noreply.github.com>
2022-09-03 23:24:42 +0300

    samurayWay 22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import style from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 
 export const Dialogs = () => {
     return (
         <div className={style.dialogs}>
             <div className={style.dialogItems}>
-                <div className={style.dialog + " " + style.active}>name 1</div>
-                <div className={style.dialog}>name 2</div>
-                <div className={style.dialog}>name 3</div>
+                <div className={style.dialog + " " + style.active}>
+                   <NavLink to="/dialogs/1">name 1</NavLink>
+                </div>
+                <div className={style.dialog}>
+                   <NavLink to="/dialogs/2">name 2</NavLink>
+                </div>
+                <div className={style.dialog}>
+                   <NavLink to="/dialogs/3">name 3</NavLink>
+                </div>
 
             </div>
             <div className={style.messages}>
