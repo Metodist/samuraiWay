114ba583e14773c91c1d85e5dc7ff688407c0cee
Iona M <51836963+Metodist@users.noreply.github.com>
2022-09-03 23:13:05 +0300

    samurayWay 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,19 @@ import style from "./Dialogs.module.css"
 
 export const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogItems}>
+                <div className={style.dialog + " " + style.active}>name 1</div>
+                <div className={style.dialog}>name 2</div>
+                <div className={style.dialog}>name 3</div>
+
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>text 1</div>
+                <div className={style.message}>text 2</div>
+                <div className={style.message}>text 3</div>
+
+            </div>
         </div>
     )
 }
